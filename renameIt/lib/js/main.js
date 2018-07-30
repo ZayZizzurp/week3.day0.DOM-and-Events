7babf8f7df4e6e10a27533c61e3df4845f98481e
ZayZizzurp <zker49@gmail.com>
2018-07-30 11:50:42 -0400

    completed RenameIt

---
+++ renameIt/lib/js/main.js
@@ -0,0 +1,5 @@
+const itemName = document.querySelector("h1");
+itemName.innerHTML = "Frostfang";
+
+const itemImage = document.getElementById("item_img");
+itemImage.src = "assets/Frostfang_item.png";
